930fa23417f9efc9e9980e15627c1eb8e90b4c33
Ashish R <107366609+Username1unavailable@users.noreply.github.com>
2024-03-23 16:48:30 -0700

    w

--- components/FAQBox.tsx
+++ components/FAQBox.tsx
@@ -1,8 +1,8 @@
 export const FAQBox = ({ header, text }: any) => {
   return (
-    <div className="bg-white text-black border border-accent p-4">
-      <p className="font-black text-lg">{header}</p>
-      <p className="mt-1">{text}</p>
-    </div>
+    <div className="text-black border border-accent p-4" style={{backgroundColor: "#f5efd6"}}>
+  <p className="font-black text-lg">{header}</p>
+  <p className="mt-1">{text}</p>
+</div>
   );
 };
