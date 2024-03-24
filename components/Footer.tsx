029c058b6b0dda24aa45f5561a15f88c17fb5f95
Ashish R <107366609+Username1unavailable@users.noreply.github.com>
2024-03-23 19:45:27 -0700

    we

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,11 +4,7 @@ export const Footer = () => {
       className="w-full px-4 py-2 bg-accent text-center relative"
       style={{ zIndex: 99999 }}
     >
-      An event by{" "}
-      <a href="https://thebapi.org" className="underline">
-        the BAPI
-      </a>
-      , brought to you by Bay Area highschoolers.
+     
     </div>
   );
 };
